dec5cf0a86cf7ee0506cf884e59053b0830024bf
Nolan Filter <nolanfilter@Nolans-MacBook-Pro.local>
2014-09-22 23:39:09 -0400

    Added server logic

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : Photon.MonoBehaviour {
 
 	public float speed;
 
@@ -43,18 +43,7 @@ public class PlayerController : MonoBehaviour {
 
 	void Update()
 	{
-		if( movementVector != Vector3.zero )
-		{
-			movementVector = movementVector.normalized * speed * Time.deltaTime;
-
-			transform.position += movementVector;
-			transform.rotation = Quaternion.Lerp( transform.rotation, Quaternion.LookRotation( movementVector ), 0.1f );
-		}
-			
-		cameraTransform.position = transform.TransformPoint( cameraPositionOffset );
-		cameraTransform.rotation = transform.rotation * cameraRotationOffset;
-
-		movementVector = Vector3.zero;
+		InputMovement();
 	}
 
 	void OnTriggerEnter( Collider collider )
@@ -87,6 +76,23 @@ public class PlayerController : MonoBehaviour {
 				transform.LookAt( transform.position + fromDoorTransform.forward, Vector3.up );
 			}
 		}
+	}
+
+	private void InputMovement()
+	{
+		if( movementVector != Vector3.zero )
+		{
+			movementVector = movementVector.normalized * speed * Time.deltaTime;
+			
+			transform.position += movementVector;
+
+			transform.rotation = Quaternion.Lerp( transform.rotation, Quaternion.LookRotation( movementVector ), 0.1f );
+		}
+
+		cameraTransform.position = transform.TransformPoint( cameraPositionOffset );
+		cameraTransform.rotation = transform.rotation * cameraRotationOffset;
+		
+		movementVector = Vector3.zero;
 	}
 
 	//event handlers
@@ -147,93 +153,4 @@ public class PlayerController : MonoBehaviour {
 
 	}
 	//end event handlers
-	
-	/*
-	public float speed = 10f;
-	
-	private float lastSynchronizationTime = 0f;
-	private float syncDelay = 0f;
-	private float syncTime = 0f;
-	private Vector3 syncStartPosition = Vector3.zero;
-	private Vector3 syncEndPosition = Vector3.zero;
-
-	void Awake()
-	{
-		lastSynchronizationTime = Time.time;
-	}
-
-	void Update()
-	{
-		if( networkView.isMine )
-		{
-			InputMovement();
-			InputColorChange();
-		}
-		else
-		{
-			SyncedMovement();
-		}
-	}
-
-	private void InputMovement()
-	{
-		if( Input.GetKey( KeyCode.UpArrow ) )
-			rigidbody.MovePosition( rigidbody.position + Vector3.forward * speed * Time.deltaTime );
-
-		if( Input.GetKey( KeyCode.DownArrow ) )
-			rigidbody.MovePosition( rigidbody.position - Vector3.forward * speed * Time.deltaTime );
-
-		if( Input.GetKey( KeyCode.RightArrow ) )
-			rigidbody.MovePosition( rigidbody.position + Vector3.right * speed * Time.deltaTime );
-
-		if( Input.GetKey( KeyCode.LeftArrow ) )
-			rigidbody.MovePosition( rigidbody.position - Vector3.right * speed * Time.deltaTime );
-	}
-
-	void OnSerializeNetworkView( BitStream stream, NetworkMessageInfo info )
-	{
-		Vector3 syncPosition = Vector3.zero;
-		Vector3 syncVelocity = Vector3.zero;
-		if (stream.isWriting)
-		{
-			syncPosition = rigidbody.position;
-			stream.Serialize(ref syncPosition);
-			
-			syncPosition = rigidbody.velocity;
-			stream.Serialize(ref syncVelocity);
-		}
-		else
-		{
-			stream.Serialize(ref syncPosition);
-			stream.Serialize(ref syncVelocity);
-			
-			syncTime = 0f;
-			syncDelay = Time.time - lastSynchronizationTime;
-			lastSynchronizationTime = Time.time;
-			
-			syncEndPosition = syncPosition + syncVelocity * syncDelay;
-			syncStartPosition = rigidbody.position;
-		}
-	}
-
-	private void SyncedMovement()
-	{
-		syncTime += Time.deltaTime;
-		rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
-	}
-
-	private void InputColorChange()
-	{
-		if( Input.GetKeyDown( KeyCode.C ) )
-			ChangeColorTo( new Vector3( Random.value, Random.value, Random.value ) );
-	}
-
-	[RPC] void ChangeColorTo( Vector3 color )
-	{
-		renderer.material.color = new Color( color.x, color.y, color.z, 1f );
-
-		if( networkView.isMine )
-			networkView.RPC( "ChangeColorTo", RPCMode.Others, color );
-	}
-	*/
 }
