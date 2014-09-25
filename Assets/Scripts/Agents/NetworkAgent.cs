ef328efa49e3431fd10edcd02afae207e25fd99e
Nolan Filter <nolanfilter@172-16-31-46.dynapool.nyu.edu>
2014-09-25 18:00:05 -0400

    Updated Control Scheme Updated Server code

--- Assets/Scripts/Agents/NetworkAgent.cs
+++ Assets/Scripts/Agents/NetworkAgent.cs
@@ -4,13 +4,55 @@ using System.Collections;
 public class NetworkAgent : MonoBehaviour {
 
 	public GameObject playerPrefab;
+	public GameObject networkBackgroundPrefab;
 
 	private const string roomName = "MPHorror_";
 	private RoomInfo[] roomsList;
 
+	private GameObject networkBackground;
+
+	private static NetworkAgent mInstance = null;
+	public static NetworkAgent instance
+	{
+		get
+		{
+			return mInstance;
+		}
+	}
+	
+	void Awake()
+	{
+		if( mInstance != null )
+		{
+			Debug.LogError( string.Format( "Only one instance of NetworkAgent allowed! Destroying:" + gameObject.name +", Other:" + mInstance.gameObject.name ) );
+			return;
+		}
+		
+		mInstance = this;
+	}
+
 	void Start()
 	{
 		PhotonNetwork.ConnectUsingSettings( "0.1" );
+
+		if( networkBackgroundPrefab )
+		{
+			networkBackground = Instantiate( networkBackgroundPrefab ) as GameObject;
+			networkBackground.transform.parent = Camera.main.transform;
+			networkBackground.transform.localPosition = new Vector3( 0f, 0f, Camera.main.nearClipPlane + 0.001f );
+			networkBackground.transform.localRotation = Quaternion.identity;
+		}
+	}
+
+	void Update()
+	{
+		if( Input.GetKeyDown( KeyCode.Return ) )
+		{
+			LeaveRoom();
+		}
+
+		if( networkBackground != null )
+			networkBackground.renderer.enabled = ( PhotonNetwork.room == null );
 	}
 
 	void OnGUI()
@@ -24,13 +66,13 @@ public class NetworkAgent : MonoBehaviour {
 			// Create Room
 			if( GUI.Button( new Rect( 100, 100, 250, 100 ), "Start Server" ) )
 				PhotonNetwork.CreateRoom( roomName + System.Guid.NewGuid().ToString( "N" ), true, true, 5 );
-			
+				
 			// Join Room
 			if( roomsList != null )
 			{
 				for (int i = 0; i < roomsList.Length; i++ )
 				{
-					if( GUI.Button( new Rect( 100, 250 + ( 110 * i ), 250, 100 ), "Join " + roomsList[i].name ) )
+					if( GUI.Button( new Rect( 250, 100 + ( 110 * i ), 250, 100 ), "Join " + roomsList[i].name ) )
 						PhotonNetwork.JoinRoom( roomsList[i].name );
 				}
 			}
@@ -49,98 +91,8 @@ public class NetworkAgent : MonoBehaviour {
 			PhotonNetwork.Instantiate ( playerPrefab.name, Vector3.up * 5, Quaternion.identity, 0 );
 	}
 
-	/*
-	private const string typeName = "UniqueGameName";
-	private const string gameName = "MPHorror";
-
-	public GameObject playerPrefab;
-
-	private bool isRefreshingHostList = false;
-	private HostData[] hostList;
-
-	private int attemptedConnectionHostIndex = -1;
-
-	void Awake()
+	public static void LeaveRoom()
 	{
-		//MasterServer.ipAddress = "127.0.0.1";
-	}
-
-	void OnGUI()
-	{
-		if( !Network.isClient && !Network.isServer )
-		{
-			if( GUI.Button( new Rect( 100, 100, 250, 100 ), "Start Server" ) )
-				StartServer();
-			
-			if( GUI.Button( new Rect( 100, 250, 250, 100 ), "Refresh Hosts" ) )
-				RefreshHostList();
-			
-			if( hostList != null )
-			{
-				for( int i = 0; i < hostList.Length; i++ )
-				{
-					if( attemptedConnectionHostIndex != i )
-					{
-						if( GUI.Button( new Rect( 400, 100 + (110 * i), 300, 100), hostList[i].gameName ) )
-						{
-							attemptedConnectionHostIndex = i;
-							JoinServer(hostList[i]);
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	private void StartServer()
-	{
-		Network.InitializeServer( 4, 25001, !Network.HavePublicAddress() );
-		MasterServer.RegisterHost( typeName, gameName );
-	}
-	
-	void OnServerInitialized()
-	{
-		SpawnPlayer();
-	}
-	
-	
-	void Update()
-	{
-		if( isRefreshingHostList && MasterServer.PollHostList().Length > 0 )
-		{
-			isRefreshingHostList = false;
-			hostList = MasterServer.PollHostList();
-		}
-	}
-	
-	private void RefreshHostList()
-	{
-		if( !isRefreshingHostList )
-		{
-			isRefreshingHostList = true;
-			MasterServer.RequestHostList( typeName );
-		}
-	}
-	
-	private void JoinServer( HostData hostData )
-	{
-		Network.Connect( hostData );
-	}
-	
-	void OnConnectedToServer()
-	{
-		attemptedConnectionHostIndex = -1;
-		SpawnPlayer();
-	}
-
-	void OnFailedToConnect()
-	{
-		attemptedConnectionHostIndex = -1;
-	}
-	
-	private void SpawnPlayer()
-	{
-		Network.Instantiate(playerPrefab, Vector3.zero, Quaternion.identity, 0);
-	}
-	*/
+		PhotonNetwork.LeaveRoom();
+	}	
 }
