e0d7da2dd59cfa8dc05880d138af00168279c572
Nolan Filter <nolanfilter@172-16-28-158.dynapool.nyu.edu>
2014-10-23 15:07:56 -0400

    Added scripts for affecting player fear and sanity from objects

---
+++ Assets/Scripts/Controllers/FearObjectController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FearObjectController : MonoBehaviour {
+
+	public float fearChange = 0.3f;
+	public bool damageOverTime = false;
+
+	void Start()
+	{
+		fearChange = Mathf.Clamp( fearChange, -1f, 1f );
+	}
+
+	void OnTriggerEnter( Collider collider )
+	{
+		EvaluateCollider( collider );
+	}
+	
+	void OnTiggerStay( Collider collider )
+	{
+		EvaluateCollider( collider );
+	}
+	
+	private void EvaluateCollider( Collider collider )
+	{
+		if( collider.tag == "Player" )
+		{
+			PlayerController playerController = collider.GetComponent<PlayerController>();
+			
+			if( playerController == null || playerController.GetCurrentState() != PlayerController.State.Normal )
+				return;
+
+			if( damageOverTime )
+				playerController.ChangeFear( fearChange * Time.deltaTime );
+			else
+				playerController.ChangeFear( fearChange );
+		}
+	}
+}
