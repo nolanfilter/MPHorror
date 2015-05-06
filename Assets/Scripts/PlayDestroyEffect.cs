5401f1adf6cbc1035f36e324257815e99015becd
Bruce Lan <Bruce@darkwindmedia.com>
2015-05-06 00:12:29 -0400

    Implemented the mannequin destroy effects

---
+++ Assets/Scripts/PlayDestroyEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayDestroyEffect : MonoBehaviour {
+
+	public GameObject particleEffect;
+	
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	public void PlayEffect()
+	{
+		//Debug.Log("Particles!");
+		GameObject particle = Instantiate(particleEffect, transform.position, Quaternion.identity) as GameObject;
+		particle.GetComponent<ParticleSystem>().Play();
+	}
+}
